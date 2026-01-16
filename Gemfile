edbb7a7032b2b60932e074f6dc23ac30d502eed7
Timofey Shufletyuk <qotofey@gmail.com>
2026-01-16 19:30:05 +0300

    Feat: Login by username & JWT session

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "image_processing", "~> 1.2"
 
 gem "jsonapi-serializer", "~> 2.2"
 gem "jsonapi_parameters", "~> 2.3"
+gem "jwt", "~> 3.1"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -5,6 +5,7 @@ module Api
     class ApplicationController < ActionController::API
       rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
       rescue_from ActionController::RoutingError, with: :render_not_found
+      rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
 
       # def render_not_found(exception)
       #   render json: { error: exception.message }, status: :not_found
@@ -12,12 +13,28 @@ module Api
 
       private
 
+      def render_unprocessable_entity(exception)
+        field_name = exception.param
+
+        binding.irb
+        render json: {
+          errors: [
+            {
+              status: "422",
+              source: { pointer: "/#{field_name}" },
+              title: "",
+              detail: exception.message
+            }
+          ]
+        }, status: :unprocessable_entity
+      end
+
       def render_not_found
         render json: {
           jsonapi: { version: "1.1" },
           errors: [
             {
-              status: 404,
+              status: "404",
               title: "Not Found",
               detail: "Page not found"
             }
@@ -30,7 +47,7 @@ module Api
           jsonapi: { version: "1.1" },
           errors: [
             {
-              status: 401,
+              status: "401",
               title: "Forbidden"
             }
           ]
